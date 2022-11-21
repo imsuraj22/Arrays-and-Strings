6e4d560ec69b90193609a33c457544ead2d381e6
Suraj <surajgaikwad0287@gmail.com>
2022-11-21 20:40:00 +0530

    done

---
+++ ExitFromEntrances.java
@@ -0,0 +1,65 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Pair{
+    int row;
+    int col;
+    Pair(int row, int col){
+        this.row=row;
+        this.col=col;
+    }
+}
+
+public class ExitFromEntrances{
+
+    public int nearestExit(char[][] maze, int[] entrance) {
+
+        int m=maze[0].length;
+        int n=maze.length;
+
+        Queue<Pair> q =new LinkedList<Pair>();
+        Pair p=new Pair(entrance[0], entrance[1]);
+        q.add(p);
+        maze[entrance[0]][entrance[1]]='+';
+        int level=0;
+
+        int dr[]={1,-1,0,0};
+        int dc[]={0,0,-1,1};
+
+        while(!q.isEmpty()){
+            int size=q.size();
+            level++;
+            for(int i=0;i<size;i++){
+                Pair temp=q.poll();
+                int row=p.row;
+                int col=p.col;
+
+                for(int j=0;j<4;j++){
+                    int new_row=row+dr[j];
+                    int new_col=col+dc[j];
+
+                    if(new_row>=0 && new_col>=0 && new_row<n && new_col<m ){
+                        if(maze[new_row][new_col]=='+'){
+                            continue;
+                        }
+
+                        if(new_row==0 || new_col ==0 || new_row==n-1 || new_col==m-1){
+                            return level;
+                        }
+
+                        Pair s=new Pair(new_row, new_col);
+                        q.add(s);
+                        maze[new_row][new_col]='+';
+                    }
+                }
+            }
+        }
+        return -1;
+
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+    
+}
--- Test.java
+++ Test.java
@@ -1,19 +1,9 @@
-import java.util.Scanner;
+
 
 public class Test {
   public static void main(String[] args) {
-    Scanner sc=new Scanner (System.in);
-    int a,b;
-    a=sc.nextInt();
-    b=sc.nextInt();
-    
-    int result=findGcd(a,b);
-    System.out.println(result);
-}
-static int findGcd(int a , int b){
-    if(b==0){
-        return a;
+    for(int i=0;i<1;i++){
+        System.out.println(i+' ');
     }
-    return findGcd(b,a%b);
 }
-}
+} 
