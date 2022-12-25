d27d79b53ddc59e14a937b43932b3c0c4055b71c
Suraj <surajgaikwad0287@gmail.com>
2022-12-25 12:00:24 +0530

    Tabulation

--- FrogJumpWithKDistance.java
+++ FrogJumpWithKDistance.java
@@ -3,21 +3,26 @@ import java.util.Arrays;
  public class FrogJumpWithKDistance {
     static int solve(int n, int[] height , int k){
         int dp[]=new int[n];
+    
         Arrays.fill(dp,-1);
-        return solveUtil(n-1, height, dp, k);
+        return solveUtil(n, height, dp, k);
     }
 
-    static int solveUtil(int ind,int height[], int dp[], int k){
-        if(ind==0) return 0;
-        if(dp[ind]!=-1) return dp[ind];
-        int minStep=Integer.MAX_VALUE;
-        for(int j=1;j<=k;j++){
-            if(ind-j>=0){
-                int jump=solveUtil(ind-j, height, dp, k)+Math.abs(height[ind]-height[ind-j]);
-                minStep=Math.min(jump, minStep);
+    static int solveUtil(int n,int height[], int dp[], int k){
+        dp[0]=0;
+        
+        for(int i=1;i<n;i++){
+            int minSteps=Integer.MAX_VALUE;
+            for(int j=1;j<=k;j++){
+                if(i-j>=0){
+                    int jump=dp[i-j]+Math.abs(height[i]-height[i-j]);
+                    minSteps=Math.min(jump, minSteps);
+                }
             }
-        }
-        return minStep;
+            dp[i]=minSteps;
+        }   
+        return dp[n-1];
+        
     }
     
     public static void main(String args[]) {
