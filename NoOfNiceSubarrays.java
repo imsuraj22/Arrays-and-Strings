eb01ff1c851ebcff392e0102946b6f6070464cfb
Suraj <surajgaikwad0287@gmail.com>
2023-02-07 20:21:37 +0530

    No of nice subarrays

---
+++ NoOfNiceSubarrays.java
@@ -0,0 +1,30 @@
+
+import java.util.HashMap;
+
+public class NoOfNiceSubarrays {
+    static public int numberOfSubarrays(int[] nums, int k) {
+    
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%2!=0) nums[i]=1;
+            else nums[i]=0;
+         }
+         HashMap<Integer,Integer> map=new HashMap<>();
+    
+         int sum=0;
+            int count=0;
+         map.put(0, 1);
+         for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            if(map.containsKey(sum-k)){
+                count+=map.get(sum-k);
+            }
+                map.put(sum, map.getOrDefault(sum, 0)+1);
+            
+         }
+         return count;
+    }
+    public static void main(String[] args) {
+        int arr[]=new int[]{2,2,2,1,2,2,1,2,2,2};
+        System.out.println(numberOfSubarrays(arr, 2));
+    }
+}
